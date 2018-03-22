96298f8585f940ba5d9ee8e43caf14d3837a609a
Sreelal <sreelal8026@gmail.com>
2018-03-22 17:44:27 +0530

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  encapsulation: ViewEncapsulation.None, 
 })
 export class HomePage {
+   public styleX:any={ 'login__submit': true, 'processing': false ,'success ':false};
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+
+  loginChek(){
+   // alert('Hello');
+    //this.styleX=true;
+    setTimeout(() => {
+      this.styleX =  { 'login__submit': false, 'processing': true,'success ':false };
+      setTimeout(() => {
+        this.styleX =  { 'login__submit': false, 'processing': false,'success ':true };
+      },100);
+  }, 100)
   }
 
 }
